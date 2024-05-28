2cf7d9a91024d90629a4a474706655467febb966
Melanie Cortez <melaniecortez1512@gmail.com>
2024-05-28 17:22:15 -0500

    final

--- example.py
+++ example.py
@@ -1,9 +1,25 @@
-def get_coordinates(query):
-    city, country = query.split(',')
+import requests
+import json
+from decimal import Decimal
+import urllib.parse
 
-    if query == "not covered":
-        return 1, 1
-    if query == "Lima,Peru":
-        return -12.0621065, -77.0365256
-    if query == "New York City,USA"
+def get_coordinates(query):
+    encoded_query = urllib.parse.quote(query)
+    url = f"https://nominatim.openstreetmap.org/search?q={encoded_query}&format=json"
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (compatible; YourAppName/1.0; +http://yourwebsite.com)'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if (len(data) == 0):
+            return 1, 1
+        lat = data[0]["lat"]
+        lon = data[0]["lon"]
+        return lat, lon
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
     return 0, 0
--- test_structural.py
+++ test_structural.py
@@ -19,7 +19,34 @@ def test_cannot_call_non_existing_endpoints_of_the_API():
 # raises exception when the testcase does not give correct answer
 def test_the_result_is_correct_for_simple_cases():
     ret = example.get_coordinates("Lima,Peru")
-    assert (ret == (-12.0621065, -77.0365256)), "Incorrect answer"
+    assert (ret == ("-12.0621065", "-77.0365256")), "Incorrect answer"
 
 def test_the_result_is_correct_for_all_inputs():
+    # lima
     detected = example.get_coordinates("Lima,Peru")
+    expected = "-12.0621065", "-77.0365256"
+    assert (detected == expected), "Incorrect answer for Lima, Peru"
+    # nyc
+    detected = example.get_coordinates("New York,USA")
+    expected = "40.7127281", "-74.0060152"
+    assert (detected == expected), "Incorrect answer for New York, USA"
+
+    # london
+    detected = example.get_coordinates("London,UK")
+    expected = "51.5074456", "-0.1277653"
+    assert (detected == expected), "Incorrect answer for London, UK"
+
+    # paris
+    detected = example.get_coordinates("Paris,France")
+    expected = "48.8588897", "2.3200410217200766"
+    assert (detected == expected), "Incorrect answer for Paris, France"
+
+    # barcelona
+    detected = example.get_coordinates("Barcelona,Spain")
+    expected = "41.3828939", "2.1774322"
+    assert (detected == expected), "Incorrect answer for Barcelona, Spain"
+
+    # melbourne
+    detected = example.get_coordinates("Melbourne,Australia")
+    expected = "-37.8142454", "144.9631732"
+    assert (detected == expected), "Incorrect answer for Melbourne, Australia"
