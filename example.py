8032b0f82a7a4401667181418968d57085f025c5
Melanie Cortez <melaniecortez1512@gmail.com>
2024-05-28 09:27:28 -0500

    new test

--- example.py
+++ example.py
@@ -1,4 +1,9 @@
 def get_coordinates(query):
+    city, country = query.split(',')
+
     if query == "not covered":
         return 1, 1
+    if query == "Lima,Peru":
+        return -12.0621065, -77.0365256
+    if query == "New York City,USA"
     return 0, 0
--- test_structural.py
+++ test_structural.py
@@ -8,9 +8,18 @@ def test_can_call_existing_endpoints_of_theAPI():
     except:
         assert False, "Exception while calling an existing function"
 
+# raises exception when the endpoint exists
 def test_cannot_call_non_existing_endpoints_of_the_API():
     try:
         ret = example.something_not_existent("blah blah")
         assert False, "Exception not raised"
     except:
         pass
+
+# raises exception when the testcase does not give correct answer
+def test_the_result_is_correct_for_simple_cases():
+    ret = example.get_coordinates("Lima,Peru")
+    assert (ret == (-12.0621065, -77.0365256)), "Incorrect answer"
+
+def test_the_result_is_correct_for_all_inputs():
+    detected = example.get_coordinates("Lima,Peru")
