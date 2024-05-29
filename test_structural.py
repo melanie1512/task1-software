f4b2bca4b9cbd16ef32d4774317116ca8135cb6e
Melanie Cortez <melaniecortez1512@gmail.com>
2024-05-28 21:48:49 -0500

    yml final

--- test_structural.py
+++ test_structural.py
@@ -5,12 +5,6 @@ import example
 def test_can_call_existing_endpoints_of_theAPI():
     try:
         ret = example.get_coordinates("Lima,Peru")
-
-
-
-
-
-        
         assert ret is not None
     except:
         assert False, "Exception while calling an existing function"
