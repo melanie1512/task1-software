5928ea84b1a9dc9269f2808c039b69ce1e1ba6df
Melanie Cortez <melaniecortez1512@gmail.com>
2024-05-28 21:46:37 -0500

    prueba pipelines

--- test_structural.py
+++ test_structural.py
@@ -5,6 +5,12 @@ import example
 def test_can_call_existing_endpoints_of_theAPI():
     try:
         ret = example.get_coordinates("Lima,Peru")
+
+
+
+
+
+        
         assert ret is not None
     except:
         assert False, "Exception while calling an existing function"
